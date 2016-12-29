f4d59d8ae6690486734c94f9e073a56aed0fa673
Jędrzej Hasiura <Jdrzej Hasiura>
2016-12-29 22:28:22 +0100

    Initial setting up

---
+++ desktop/src/com/hyphen/wdi/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hyphen.wdi.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hyphen.wdi.WDIGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new WDIGame(), config);
+	}
+}
